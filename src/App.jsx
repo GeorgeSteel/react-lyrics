d57706531badaa0a847ea5de9465b71bd8c4286d
Jorge <julianacero98@gmail.com>
2019-05-01 19:47:37 -0500

    lyrics API request complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import Form from './components/Form';
 
+import axios from 'axios';
+import Song from './components/Song';
+
 function App() {
   // use 3 different states
   const [artist, setArtist] = useState('');
@@ -8,9 +11,12 @@ function App() {
   const [data, setData] = useState({});
 
   // request songs API method
-  const requestLyricsAPI = search => {
-    const url = `https://api.lyrics.ovh/v1/${ search.artist }/${ search.cancion }`;
+  const requestLyricsAPI = async search => {
+    const { artista, cancion } = search;
+    const url = `https://api.lyrics.ovh/v1/${ artista }/${ cancion }`;
+    const resp = await axios(url);
 
+    setLyrics(resp.data.lyrics);
   }
 
   return (
@@ -18,6 +24,18 @@ function App() {
       <Form
         requestLyricsAPI={ requestLyricsAPI }
       />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+
+          </div>
+          <div className="col-md-6">
+            <Song
+              lyrics={ lyrics }
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
---
+++ src/components/Song.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+function Song({lyrics}) {
+    if(lyrics.length === 0) return null;
+    
+    return(
+        <Fragment>
+            <h2>Letra</h2>
+            <p className="letra">
+                { lyrics }
+            </p>            
+        </Fragment>
+    )
+}
+
+export default Song;
